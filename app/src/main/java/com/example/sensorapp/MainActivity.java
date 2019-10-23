1b9ba18bc08457cccaffdb9546285723cb31e63d
unknown <kamczarniecki@gmail.com>
2019-10-23 23:03:03 +0200

    added empty button

--- app/src/main/java/com/example/sensorapp/MainActivity.java
+++ app/src/main/java/com/example/sensorapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -14,13 +15,14 @@ public class MainActivity extends AppCompatActivity
     SensorManager sMgr;
     TextView sensorText;
     List<Sensor> mList;
+    Button lightFunctionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sensorText=(TextView) findViewById(R.id.sensorText);
-
+        lightFunctionButton= (Button) findViewById(R.id.button);
         initializeAndGetAllSensors();
         printAllSensorsOfDevice();
 
