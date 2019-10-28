1d29eeea4f7210e44496657be8b7bfd4df3e1b94
unknown <kamczarniecki@gmail.com>
2019-10-28 22:59:57 +0100

    Proximity sensor distance printing

--- app/src/main/java/com/example/sensorapp/lightFunctionActivity.java
+++ app/src/main/java/com/example/sensorapp/lightFunctionActivity.java
@@ -1,5 +1,10 @@
 package com.example.sensorapp;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,27 +14,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
 
-public class lightFunctionActivity extends AppCompatActivity {
-    MainActivity mainObject;
+import org.w3c.dom.Text;
+
+public class lightFunctionActivity extends AppCompatActivity implements SensorEventListener {
+    TextView proximityTextView;
+    SensorManager sMgr;
+    Sensor proximitySensor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light_function);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-
-            }
-        });
+        proximityTextView=(TextView) findViewById(R.id.proximityTextViewId);
+        sMgr=(SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        proximitySensor= sMgr.getDefaultSensor(Sensor.TYPE_PROXIMITY);
     }
 
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        proximityTextView.setText("value:" + event.values[0]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sMgr.registerListener(this,proximitySensor,SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sMgr.unregisterListener(this);
+    }
 }
